0ab6537de0fa18cab739d69b523191e88a0bdb5f
KEINOS <github@keinos.com>
2019-04-27 11:00:06 +0900

    initial commit :tada:

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Dockerfile_of_Alpine
-❌ Simple Alpine docker image to trigger the "auto-build" of other docker images.
+# Dockerfile of Alpine
+
+Simple Alpine docker image to trigger the "auto-build" of other docker images.
+
+- Docker image: `keinos/alpine:latest`
+
+If this image was used in 'FROM' instruction of one's Dockerfile then auto-build of one's image must run when this image was updated.
---
+++ update.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+# This script checks the latest Alpine docker image version
+# and updates keinos/alpine:latest.
+#
+# NOTE: This script must run on local and only was tested with macOS(Mojave).
+#
+# Update steps:
+#   1. Pull alpine:latest image.
+#   2. Gets the os-release version of the image above.
+#   3. Compares the version between alpine:latest and keinos/alpine:latest.
+#   4. Re-writes the version info of the Dockerfile.
+#   5. Git add, commit and push if updated.
+
+# Define Basic Variables
+PATH_FILE_VER_INFO='VERSION.txt'
+
+# Load current version info
+. ./$PATH_FILE_VER_INFO
+echo '- Current version:' $VERSION_ID
+
+# Pull latest Alpine image
+docker pull alpine:latest > /dev/null
+
+# Fetch the latest Alpine version
+VERSION_NEW=$(docker run --rm -i alpine:latest cat /etc/os-release | grep VERSION_ID | sed -e 's/[^0-9\.]//g')
+echo '- Latest version:' $VERSION_NEW
+
+# Compare
+if [ $VERSION_ID = $VERSION_NEW ]; then
+   echo 'Same version. Do nothing.'
+   exit 0
+fi
+
+#  Update
+# --------
+echo 'Newer version found. Updating ...'
+
+# Updating VERSION.txt
+echo "VERSION_ID=${VERSION_NEW}" > ./$PATH_FILE_VER_INFO
+if [ $? -ne 0 ]; then
+  echo "* Failed update: ${PATH_FILE_VER_INFO}"
+  exit 1
+fi
+echo "- Updated: ${PATH_FILE_VER_INFO}"
+
+# Updating Dockerfile
+sed -i.previous -E "s/[0-9]+\.[0-9]+\.[0-9]/${VERSION_NEW}/" ./Dockerfile
+if [ $? -ne 0 ]; then
+  echo '* Failed update: Dockerfile'
+  exit 1
+fi
+echo "- Updated: Dockerfile"
+
+# Updating git
+echo 'Commit and pushing to GitHub ...'
+git add . && \
+git commit -m "feat: v${VERSION_NEW}" && \
+git tag "v{$VERSION_NEW}" && \
+git push origin
+if [ $? -ne 0 ]; then
+  echo '* Failed commit and push'
+  exit 1
+fi
+echo "- Pushed: GitHub"
+
+echo "Now wait until the Docker Hub's automated build finishes build the image."
