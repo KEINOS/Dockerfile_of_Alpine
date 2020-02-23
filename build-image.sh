a03f9b1686ede311057219474d60b411034bcfdb
KEINOS <github@keinos.com>
2020-02-23 15:54:53 +0900

    feature: multi-arch building

--- build-image.sh
+++ build-image.sh
@@ -11,9 +11,12 @@ This script builds Docker image for AMD64, ARM v6 and ARM v7 architecture. Then
 pushes to Docker Hub the images made.
 
 - Requirements:
-    1. Experimental option must be enabled. (buildx command must be available to use)
-    2. when "docker buildx ls" is run, below platforms must be listed:
-        - linux/arm/v6, linux/arm/v7, linux/arm64
+    1. Experimental option must be enabled. (buildx command must be available to use as well)
+    2. When running "docker buildx ls", the below platforms must be listed:
+        - linux/arm/v6
+        - linux/arm/v7
+        - linux/arm64
+        - linux/amd64
 
 ===============================================================================
 
@@ -32,6 +35,62 @@ PATH_FILE_VER_INFO='VERSION.txt'
 NAME_BUILDER=mybuilder
 
 # -----------------------------------------------------------------------------
+#  Functions
+# -----------------------------------------------------------------------------
+
+function build_push_pull_image () {
+    echo "- BUILDING ${NAME_PLATFORM}"
+    docker buildx build \
+        --build-arg NAME_BASE=$NAME_BASE \
+        --build-arg VER_ALPINE="v${VERSION_OS}" \
+        --platform $NAME_PLATFORM \
+        -t "${NAME_IMAGE}:${NAME_TAG}" \
+        --push . && \
+    echo "  PULLING BACK: ${NAME_IMAGE}:${NAME_TAG}" && \
+    docker pull "${NAME_IMAGE}:${NAME_TAG}"
+
+    return $?
+}
+
+function create_builder () {
+    echo '- Create builder: ' $1
+    docker buildx ls | grep $1 1>/dev/null
+    [ $? -ne 0 ] && {
+        docker buildx create --name $1
+    }
+
+    return $?
+}
+
+function create_manifest () {
+    echo '- Removing image from local:'
+    docker image rm --force $1 2>/dev/null 1>/dev/null
+    echo "- Creating manifest for: $1"
+    echo "  With images: ${2}"
+    docker manifest create $1 $2 --amend
+
+    return $?
+}
+
+function login_docker () {
+    echo -n '- Login to Docker: '
+    docker login 2>/dev/null 1>/dev/null || {
+        echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin || {
+            echo 'You need to login Docker Cloud/Hub first.'
+            exit 1
+        }
+    }
+    echo 'OK'
+}
+
+function rewrite_variant_manifest () {
+    echo "- Re-writing variant to: $3"
+    docker manifest annotate $1 $2 --variant $3
+
+    return $?
+}
+
+# -----------------------------------------------------------------------------
 #  Main
 # -----------------------------------------------------------------------------
 
@@ -40,98 +99,66 @@ source ./$PATH_FILE_VER_INFO
 VERSION_OS=$VERSION_ID
 echo '- Current Alpine version:' $VERSION_OS
 
-echo '- Login to Docker:'
-docker login 2>/dev/null 1>/dev/null || {
-    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin || {
-        echo 'You need to login Docker Cloud/Hub first.'
-        exit 1
-    }
-}
+login_docker
 
-echo '- docker buildx ls'
-docker buildx ls
-
-docker buildx ls | grep $NAME_BUILDER
-[ $? -ne 0 ] && {
-    echo '- Create builder: ' $NAME_BUILDER
-    docker buildx create --name $NAME_BUILDER
-}
+create_builder $NAME_BUILDER
 
 echo '- Start build:'
 docker buildx use $NAME_BUILDER
 docker buildx inspect --bootstrap
 
 # Build ARMv6
-NAME_TAG='arm32v6'
-docker buildx build \
-    --build-arg NAME_BASE="${NAME_TAG}/alpine" \
-    --build-arg VER_ALPINE="v${VERSION_OS}" \
-    --platform linux/arm/v6 \
-    -t "${NAME_IMAGE}:${NAME_TAG}" \
-    --push . && \
-docker pull "${NAME_IMAGE}:${NAME_TAG}"
+NAME_BASE='arm32v6/alpine'
+NAME_TAG='armv6'
+NAME_PLATFORM='linux/arm/v6'
+build_push_pull_image
 
 # Build ARMv7
-NAME_TAG='arm32v7'
-docker buildx build \
-    --build-arg NAME_BASE="${NAME_TAG}/alpine" \
-    --build-arg VER_ALPINE="v${VERSION_OS}" \
-    --platform linux/arm/v7 \
-    -t "${NAME_IMAGE}:${NAME_TAG}" \
-    --push . \
-&& docker pull "${NAME_IMAGE}:${NAME_TAG}"
+NAME_BASE='arm32v7/alpine'
+NAME_TAG='armv7'
+NAME_PLATFORM='linux/arm/v7'
+build_push_pull_image
 
 # Build AMD64
+NAME_BASE='alpine'
 NAME_TAG='amd64'
-docker buildx build \
-    --build-arg NAME_BASE="alpine" \
-    --build-arg VER_ALPINE="v${VERSION_OS}" \
-    --platform linux/amd64 \
-    -t "${NAME_IMAGE}:${NAME_TAG}" \
-    --push . \
-&& docker pull "${NAME_IMAGE}:${NAME_TAG}"
+NAME_PLATFORM='linux/amd64'
+build_push_pull_image
 
 # Build ARM64
+NAME_BASE='alpine'
 NAME_TAG='arm64'
-docker buildx build \
-    --build-arg NAME_BASE="alpine" \
-    --build-arg VER_ALPINE="v${VERSION_OS}" \
-    --platform linux/arm64 \
-    -t "${NAME_IMAGE}:${NAME_TAG}" \
-    --push . \
-&& docker pull "${NAME_IMAGE}:${NAME_TAG}"
+NAME_PLATFORM='linux/arm64'
+build_push_pull_image
 
+echo "- Inspect built image of: ${NAME_IMAGE}"
 docker buildx imagetools inspect $NAME_IMAGE
+
+echo '- Switch back builder to default:'
+docker buildx stop $NAME_BUILDER
 docker buildx use default
 
-# Create manifest list with latest tag
-NAME_TAG_LATEST="${NAME_IMAGE}:latest"
-docker image rm --force $NAME_TAG_LATEST 2>/dev/null 1>/dev/null
-docker manifest create $NAME_TAG_LATEST \
-    $NAME_IMAGE:amd64 \
-    $NAME_IMAGE:arm32v6 \
-    $NAME_IMAGE:arm32v7 \
-    $NAME_IMAGE:arm64 \
-    --amend
-docker manifest annotate $NAME_TAG_LATEST \
-    $NAME_IMAGE:arm32v6 --os linux --arch arm --variant v6l
-docker manifest annotate $NAME_TAG_LATEST \
-    $NAME_IMAGE:arm32v7 --os linux --arch arm --variant v7l
-docker manifest inspect $NAME_TAG_LATEST
-docker manifest push $NAME_TAG_LATEST --purge
+# Create manifest
+LIST_IMAGE_INCLUDE="$NAME_IMAGE:armv6 $NAME_IMAGE:armv7 $NAME_IMAGE:arm64 $NAME_IMAGE:amd64"
+
+echo "- Creating manifest for image: ${NAME_IMAGE} with: latest tag"
+NAME_IMAGE_AND_TAG="${NAME_IMAGE}:latest"
+create_manifest  $NAME_IMAGE_AND_TAG "$LIST_IMAGE_INCLUDE"
+
+rewrite_variant_manifest $NAME_IMAGE_AND_TAG $NAME_IMAGE:armv6 v6l
+rewrite_variant_manifest $NAME_IMAGE_AND_TAG $NAME_IMAGE:armv7 v7l
+
+docker manifest inspect $NAME_IMAGE_AND_TAG && \
+docker manifest push $NAME_IMAGE_AND_TAG --purge
 
 # Create manifest list with current version
-NAME_TAG_CURRENT="${NAME_IMAGE}:v${VERSION_OS}"
-docker image rm --force $NAME_TAG_CURRENT 2>/dev/null 1>/dev/null
-docker manifest create $NAME_TAG_CURRENT \
-    $NAME_IMAGE:amd64 \
-    $NAME_IMAGE:arm32v6 \
-    $NAME_IMAGE:arm32v7 \
-    $NAME_IMAGE:arm64 \
-    --amend
-docker manifest annotate $NAME_TAG_CURRENT \
-    $NAME_IMAGE:arm32v6 --os linux --arch arm --variant v6l
-docker manifest annotate $NAME_TAG_CURRENT \
-    $NAME_IMAGE:arm32v7 --os linux --arch arm --variant v7l
-docker manifest inspect $NAME_TAG_CURRENT
-docker manifest push $NAME_TAG_CURRENT --purge
+echo "- Creating manifest for image: ${NAME_IMAGE} with: v${VERSION_OS} tag"
+NAME_IMAGE_AND_TAG="${NAME_IMAGE}:v${VERSION_OS}"
+
+create_manifest  $NAME_IMAGE_AND_TAG "$LIST_IMAGE_INCLUDE"
+
+rewrite_variant_manifest $NAME_IMAGE_AND_TAG $NAME_IMAGE:armv6 v6l
+rewrite_variant_manifest $NAME_IMAGE_AND_TAG $NAME_IMAGE:armv7 v7l
+
+docker manifest inspect $NAME_IMAGE_AND_TAG && \
+docker manifest push $NAME_IMAGE_AND_TAG --purge
